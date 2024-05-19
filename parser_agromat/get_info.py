fb81c87bc15b0dbb47ba7a9dd7ad99ccb1283e56
VladimirovichOleg <olegvladimirovic18@gmail.com>
2024-05-19 20:29:22 +0300

    изменения

--- parser_agromat/get_info.py
+++ parser_agromat/get_info.py
@@ -4,7 +4,7 @@ from urllib.error import HTTPError
 from urllib.error import URLError
 
 
-class GetInfo:
+class GetResp:
     @staticmethod
     def get_bs4_obj_all_page(url: str, headers_: dict) -> object | None:
         try:
@@ -18,34 +18,19 @@ class GetInfo:
             print("URL Error")
             return None
 
+
+class GetInfoGeneralPage:
     @staticmethod
-    def get_main_tags_a_on_general_page(bs4_obj) -> object | str:
+    def get_main_tags_a(bs4_obj) -> object | str:
         return bs4_obj.find(name='div', attrs={'class': 'list-content js-cat _js-ga_view-list_catalog'}).find_all('a')
 
     @staticmethod
-    def get_main_tag_on_individual_page(bs4_obj): # div : class - product-main
-        return bs4_obj.find('div', {'class': 'product-main'})
-
-    @staticmethod
-    def get_name_product_on_general_page(bs4_obj):
+    def get_name_product(bs4_obj):
         return bs4_obj.find('span', {'class': '_js-analytics'}).get("data-name")
 
     @staticmethod
-    def get_href_product_individual_page(main_address, bs4_obj):
+    def get_href_individual_page(main_address, bs4_obj):
         return f'{main_address}{bs4_obj.get("href")}'
-
-    @staticmethod
-    def get_discount_individual_page(bs4_obj):
-        try:
-            return (bs4_obj.find('div', {'class': 'product-images'}).
-                    find('div', {'class': 'product-images__wrapper'}).find('div', {'class': 'swiper main-slider'}).
-                    find('div', {'class': 'goods-label'}).find('span').text)
-        except:
-            return 'No discount'
-
-    @staticmethod
-    def get_in_stock_individual_page(bs4_obj):
-        return bs4_obj.find('div', {'class': 'bottom'}).find('div', {'class': 'cart-goods-alert'}).text
 
     @staticmethod
     def get_now_price(bs4_obj) -> dict:
@@ -78,25 +63,48 @@ class GetInfo:
         return tag_p[1].text
 
     @staticmethod
-    def get_unit_of_value(bs4_obj):
-        return bs4_obj.find('div', {'class': 'price'}).text[-3:]
-
-    @staticmethod
     def get_href_small_picture(bs4_obj):
         return (bs4_obj.find('div', {'class': 'top'}).find('div', {'class': 'swiper'}).find('div', {'class': 'swiper-wrapper'}).
                 find('div', {'class':'swiper-slide'}).find('picture').find('img').get('src'))
 
     @staticmethod
-    def get_href_picture_on_individual_page(bs4_obj):
-        return bs4_obj.find('div', {'class': 'swiper-slide'}).find('picture').find('img').get('src')
+    def get_pagin(bs4_obj) -> int:
+        tags_a = bs4_obj.find('div', {'class': 'pagin-content'}).find_all('a')
+        tag_a = tags_a[3].text
+        return int(tag_a)
+
+
+class GetInfoIndividualPage:
+    @staticmethod
+    def get_main_tag(bs4_obj):  # div : class - product-main
+        return bs4_obj.find('div', {'class': 'product-main'})
 
     @staticmethod
-    def get_articul_on_individual_page(bs4_obj):
+    def get_name(bs4_obj):
+        return bs4_obj.find('div', {'id': 'main-info'}).find('h1').text
+
+    @staticmethod
+    def get_discount(bs4_obj):
+        try:
+            return (bs4_obj.find('div', {'class': 'product-images'}).
+                    find('div', {'class': 'product-images__wrapper'}).find('div', {'class': 'swiper main-slider'}).
+                    find('div', {'class': 'goods-label'}).find('span').text)
+        except:
+            return 'No discount'
+
+    @staticmethod
+    def get_articul(bs4_obj):
         return (bs4_obj.find('div', {'class': 'product-info'}).find('div', {'class': 'brief-characteristics'})
                 .find('div', {'class': 'brief-characteristics__item'}).find('span').text)
 
     @staticmethod
-    def get_pagin_on_general_pae(bs4_obj) -> int:
-        tags_a = bs4_obj.find('div', {'class': 'pagin-content'}).find_all('a')
-        tag_a = tags_a[3].text
-        return int(tag_a)
+    def get_href_picture(bs4_obj):
+        return bs4_obj.find('div', {'class': 'swiper-slide'}).find('picture').find('img').get('src')
+
+    @staticmethod
+    def get_unit_of_value(bs4_obj):
+        return bs4_obj.find('div', {'class': 'price'}).text[-3:]
+
+    @staticmethod
+    def get_in_stock(bs4_obj):
+        return bs4_obj.find('div', {'class': 'bottom'}).find('div', {'class': 'cart-goods-alert'}).text
