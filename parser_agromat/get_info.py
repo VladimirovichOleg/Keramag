d80611d54c34779ddad83d3163cfcdc900ddf90a
VladimirovichOleg <olegvladimirovic18@gmail.com>
2024-05-19 16:50:39 +0300

    изменения

--- parser_agromat/get_info.py
+++ parser_agromat/get_info.py
@@ -6,7 +6,7 @@ from urllib.error import URLError
 
 class GetInfo:
     @staticmethod
-    def get_bs4_obj(url: str, headers_: dict) -> object | None:
+    def get_bs4_obj_all_page(url: str, headers_: dict) -> object | None:
         try:
             resp = Request(url, headers=headers_)
             down_html = urlopen(resp)
@@ -19,31 +19,32 @@ class GetInfo:
             return None
 
     @staticmethod
-    def get_main_tags_a_in_first_page(bs4_obj) -> object | str:
+    def get_main_tags_a_on_general_page(bs4_obj) -> object | str:
         return bs4_obj.find(name='div', attrs={'class': 'list-content js-cat _js-ga_view-list_catalog'}).find_all('a')
 
     @staticmethod
-    def get_main_tag_in_second_page(bs4_obj): # div : class - product-main
+    def get_main_tag_on_individual_page(bs4_obj): # div : class - product-main
         return bs4_obj.find('div', {'class': 'product-main'})
 
     @staticmethod
-    def get_name_product(bs4_obj):
+    def get_name_product_on_general_page(bs4_obj):
         return bs4_obj.find('span', {'class': '_js-analytics'}).get("data-name")
 
     @staticmethod
-    def get_href_product(main_address, bs4_obj):
+    def get_href_product_individual_page(main_address, bs4_obj):
         return f'{main_address}{bs4_obj.get("href")}'
 
     @staticmethod
-    def get_discount_or_None(bs4_obj):
+    def get_discount_individual_page(bs4_obj):
         try:
-            return bs4_obj.find('div', {'class': 'top'}).find('div', {'class': 'swiper slider-cart-img'}).find('div', {
-                'class': 'goods-label'}).find('span').text
+            return (bs4_obj.find('div', {'class': 'product-images'}).
+                    find('div', {'class': 'product-images__wrapper'}).find('div', {'class': 'swiper main-slider'}).
+                    find('div', {'class': 'goods-label'}).find('span').text)
         except:
             return 'No discount'
 
     @staticmethod
-    def get_in_stock(bs4_obj):
+    def get_in_stock_individual_page(bs4_obj):
         return bs4_obj.find('div', {'class': 'bottom'}).find('div', {'class': 'cart-goods-alert'}).text
 
     @staticmethod
@@ -86,16 +87,16 @@ class GetInfo:
                 find('div', {'class':'swiper-slide'}).find('picture').find('img').get('src'))
 
     @staticmethod
-    def get_href_picture_in_next_pages(bs4_obj):
+    def get_href_picture_on_individual_page(bs4_obj):
         return bs4_obj.find('div', {'class': 'swiper-slide'}).find('picture').find('img').get('src')
 
     @staticmethod
-    def get_articul(bs4_obj):
+    def get_articul_on_individual_page(bs4_obj):
         return (bs4_obj.find('div', {'class': 'product-info'}).find('div', {'class': 'brief-characteristics'})
                 .find('div', {'class': 'brief-characteristics__item'}).find('span').text)
 
     @staticmethod
-    def get_pagin(bs4_obj) -> int:
+    def get_pagin_on_general_pae(bs4_obj) -> int:
         tags_a = bs4_obj.find('div', {'class': 'pagin-content'}).find_all('a')
         tag_a = tags_a[3].text
         return int(tag_a)
