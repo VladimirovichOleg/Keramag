252144c43ad76cdd368a2889c61cb2e0d8c78ea3
VladimirovichOleg <olegvladimirovic18@gmail.com>
2024-05-20 09:14:19 +0300

    изменения

--- parser_agromat/get_info.py
+++ parser_agromat/get_info.py
@@ -21,19 +21,19 @@ class GetResp:
 
 class GetInfoGeneralPage:
     @staticmethod
-    def get_main_tags_a(bs4_obj) -> object | str:
+    def main_tags_a(bs4_obj) -> object | str:
         return bs4_obj.find(name='div', attrs={'class': 'list-content js-cat _js-ga_view-list_catalog'}).find_all('a')
 
     @staticmethod
-    def get_name_product(bs4_obj):
+    def name_product(bs4_obj):
         return bs4_obj.find('span', {'class': '_js-analytics'}).get("data-name")
 
     @staticmethod
-    def get_href_individual_page(main_address, bs4_obj):
+    def href_individual_page(main_address, bs4_obj):
         return f'{main_address}{bs4_obj.get("href")}'
 
     @staticmethod
-    def get_now_price(bs4_obj) -> dict:
+    def now_price(bs4_obj) -> dict:
         price = {}
         try:
             price['price_now'] = (bs4_obj.find('div', {'class': 'bottom'}).find('div', {'class': 'price'}).
@@ -49,62 +49,62 @@ class GetInfoGeneralPage:
             return price
 
     @staticmethod
-    def get_size(bs4_obj) -> str:
+    def size(bs4_obj) -> str:
         tag_div = (bs4_obj.find('div', {'class': 'bottom'}).find('div', {'class': 'params-list'}).
                 find_all('div', {'class': 'params-list__item'}))
         tag_p = tag_div[0].find_all('p')
         return tag_p[1].text
 
     @staticmethod
-    def get_country(bs4_obj):
+    def country(bs4_obj):
         tag_div = (bs4_obj.find('div', {'class': 'bottom'}).find('div', {'class': 'params-list'}).
                    find_all('div', {'class': 'params-list__item'}))
         tag_p = tag_div[2].find_all('p')
         return tag_p[1].text
 
     @staticmethod
-    def get_href_small_picture(bs4_obj):
+    def href_small_picture(bs4_obj):
         return (bs4_obj.find('div', {'class': 'top'}).find('div', {'class': 'swiper'}).find('div', {'class': 'swiper-wrapper'}).
                 find('div', {'class':'swiper-slide'}).find('picture').find('img').get('src'))
 
     @staticmethod
-    def get_pagin(bs4_obj) -> int:
+    def pagin(bs4_obj) -> int:
         tags_a = bs4_obj.find('div', {'class': 'pagin-content'}).find_all('a')
         tag_a = tags_a[3].text
         return int(tag_a)
 
 
 class GetInfoIndividualPage:
-    @staticmethod
-    def get_main_tag(bs4_obj):  # div : class - product-main
-        return bs4_obj.find('div', {'class': 'product-main'})
 
     @staticmethod
-    def get_name(bs4_obj):
-        return bs4_obj.find('div', {'id': 'main-info'}).find('h1').text
+    def name(bs4_obj):
+        return bs4_obj.find('div', {'class': 'product-main'}).find('div', {'id': 'main-info'}).find('h1').text
 
     @staticmethod
-    def get_discount(bs4_obj):
+    def discount(bs4_obj):
         try:
-            return (bs4_obj.find('div', {'class': 'product-images'}).
+            return (bs4_obj.find('div', {'class': 'product-main'}).find('div', {'class': 'product-images'}).
                     find('div', {'class': 'product-images__wrapper'}).find('div', {'class': 'swiper main-slider'}).
                     find('div', {'class': 'goods-label'}).find('span').text)
         except:
             return 'No discount'
 
     @staticmethod
-    def get_articul(bs4_obj):
-        return (bs4_obj.find('div', {'class': 'product-info'}).find('div', {'class': 'brief-characteristics'})
+    def articul(bs4_obj):
+        return (bs4_obj.find('div', {'class': 'product-main'}).find('div', {'class': 'product-info'})
+                .find('div', {'class': 'brief-characteristics'})
                 .find('div', {'class': 'brief-characteristics__item'}).find('span').text)
 
     @staticmethod
-    def get_href_picture(bs4_obj):
-        return bs4_obj.find('div', {'class': 'swiper-slide'}).find('picture').find('img').get('src')
+    def href_picture(bs4_obj):
+        return (bs4_obj.find('div', {'class': 'product-main'}).find('div', {'class': 'swiper-slide'})
+                .find('picture').find('img').get('src'))
 
     @staticmethod
-    def get_unit_of_value(bs4_obj):
-        return bs4_obj.find('div', {'class': 'price'}).text[-3:]
+    def unit_of_value(bs4_obj):
+        return bs4_obj.find('div', {'class': 'product-main'}).find('div', {'class': 'price'}).text[-3:]
 
     @staticmethod
-    def get_in_stock(bs4_obj):
-        return bs4_obj.find('div', {'class': 'bottom'}).find('div', {'class': 'cart-goods-alert'}).text
+    def in_stock(bs4_obj):
+        return (bs4_obj.find('div', {'class': 'product-main'}).
+                find('div', {'class': 'bottom'}).find('div', {'class': 'cart-goods-alert'}).text)
--- parser_agromat/main.py
+++ parser_agromat/main.py
@@ -1,30 +1,22 @@
-
-
-from get_info import GetInfo
+from get_info import GetInfoGeneralPage, GetResp, GetInfoIndividualPage
 from config import config_file
+from headers import different_headers
 
 
 def main():
-    url = f"{config_file['DOMAIN']}{config_file['CATALOG']}"
-    headers = {
-        'Accept': '* / *',
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0',
-        'Accept-Language': 'u-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3'
-    }
+    url_start = f"{config_file['DOMAIN']}{config_file['CATALOG']}"
+    first_bs4_obj = GetResp.get_bs4_obj_all_page(url_start, headers_=different_headers['headers_1'])
 
-    first_bs4_obj = GetInfo.get_bs4_obj(url, headers_=headers)
+    for page in range(1, GetInfoGeneralPage.pagin(first_bs4_obj) + 1):
+        url_in = f"{config_file['DOMAIN']}{config_file['CATALOG']}?page={page}"
+        bs4_obj_all_general_page = GetResp.get_bs4_obj_all_page(url_in, headers_=different_headers['headers_1'])
 
-    for page in range(1, GetInfo.get_pagin(first_bs4_obj) + 1):
-        url = f"{config_file['DOMAIN']}{config_file['CATALOG']}?page={page}"
-        bs4_obj = GetInfo.get_bs4_obj(url, headers_=headers)
+        for a in GetInfoGeneralPage.main_tags_a(bs4_obj_all_general_page):
+            href_individual_page = GetInfoGeneralPage.href_individual_page(config_file['DOMAIN'], a)
+            bs4_obj_all_individual_page = GetResp.get_bs4_obj_all_page(href_individual_page, headers_=different_headers['headers_1'])
 
-        for i in GetInfo.get_main_tags_a_in_first_page(bs4_obj):
-            print(f'name - {GetInfo.get_name_product(i)}')
-            href_prod = GetInfo.get_href_product(config_file['DOMAIN'], i)
+            print(GetInfoIndividualPage.name(bs4_obj_all_individual_page))
 
-            second_bs4_obj = GetInfo.get_bs4_obj(href_prod, headers_=headers)
-            main_tag = GetInfo.get_main_tag_in_second_page(second_bs4_obj)
-            print(GetInfo.get_articul(main_tag))
 
 
 if __name__ == '__main__':
